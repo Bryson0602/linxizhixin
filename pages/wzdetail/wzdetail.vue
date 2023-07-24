60ed0c2c276b5f87fef5d85f269675591e9111ad
sunheran <1131463196@qq.com>
2023-07-24 10:56:41 +0800

    首页修改卡片样式，参数传递成功，引入时间插件,第三次打包

--- pages/wzdetail/wzdetail.vue
+++ pages/wzdetail/wzdetail.vue
@@ -3,26 +3,33 @@
 		<view v-if="loadState">			
 			<view class="title">{{detail.title}}</view>
 			<view class="info">
-				<text>{{detail.author}}</text>
+				<view >
+				      <text class="author">{{detail.author}}+</text>
+				</view>
 				<text>			
 					<uni-dateformat 
 					:date="detail.posttime" 
-					format="yyyy年MM月dd日 hh:mm:ss" 
+					format="yyyy年MM月dd日 hh:mm" 
 					></uni-dateformat>
 				</text>
 			</view>
 			<view class="content">
-				{{detail.content}}
+				<text>{{detail.content1}}</text>
+				<image :src="detail.picture1" mode="widthFix"></image>
+				<text>{{detail.content2}}</text>
+				<image :src="detail.picture2" mode="widthFix"></image>
+				<text>{{detail.content3}}</text>
+				<image :src="detail.picture3" mode="widthFix"></image>
 			</view>
 			
-			<view class="picurls" v-if="detail.picurls && detail.picurls.length">
+			<!-- <view class="picurls" v-if="detail.picurls && detail.picurls.length">
 				<image v-for="item in detail.picurls" :src="item" mode="widthFix"></image>
-			</view>
+			</view> -->
 			
-			<view class="btnGroup">
+			<!-- <view class="btnGroup">
 				<button size="mini" @click="goEdit">修改</button>
 				<button size="mini" type="warn" @click="onRemove">删除</button>
-			</view>
+			</view> -->
 		</view>
 		
 		<view v-else>
@@ -51,41 +58,42 @@
 		},
 		methods:{
 			//跳转到修改页面
-			goEdit(){
-				uni.navigateTo({
-					url:"/pages/edit/edit?id="+id
-				})
-			},
+			// goEdit(){
+			// 	uni.navigateTo({
+			// 		url:"/pages/edit/edit?id="+id
+			// 	})
+			// },
 			
 			//删除一条记录
-			onRemove(){
-				uni.showModal({
-					content:"是否确认删除？",
-					success:res=>{						
-						if(res.confirm){
-							this.removeFun()
-						}
-					}					
-				})			
+			// onRemove(){
+			// 	uni.showModal({
+			// 		content:"是否确认删除？",
+			// 		success:res=>{						
+			// 			if(res.confirm){
+			// 				this.removeFun()
+			// 			}
+			// 		}					
+			// 	})			
 				
-			},
+			// },
 			
 			removeFun(){
 				uniCloud.callFunction({
-					name:"art_remove_row",
+					//name:"art_remove_row",
 					data:{
 						id
 					}
-				}).then(res=>{
-					uni.showToast({
-						title:"删除成功~"
-					})
-					setTimeout(()=>{
-						uni.reLaunch({
-							url:"/pages/index/index"
-						})
-					},800)
 				})
+				// .then(res=>{
+				// 	uni.showToast({
+				// 		title:"删除成功~"
+				// 	})
+				// 	setTimeout(()=>{
+				// 		uni.reLaunch({
+				// 			url:"/pages/index/index"
+				// 		})
+				// 	},800)
+				// })
 			},
 			
 			
@@ -103,18 +111,19 @@
 					uni.setNavigationBarTitle({
 						title:this.detail.title
 					})
-				}).catch(()=>{
-					uni.showToast({
-						icon:"error",
-						title:"删除有误"
-					})
-					setTimeout(()=>{
-						uni.reLaunch({
-							url:"/pages/index/index"
-						})
-					},800)
-					
 				})
+				// .catch(()=>{
+				// 	uni.showToast({
+				// 		icon:"error",
+				// 		title:"删除有误"
+				// 	})
+				// 	setTimeout(()=>{
+				// 		uni.reLaunch({
+				// 			url:"/pages/index/index"
+				// 		})
+				// 	},800)
+					
+				// })
 			}
 		}
 		
@@ -127,7 +136,8 @@
 	}
 .detail{
 	padding:30rpx;
-	background-image: linear-gradient(180deg, #ccf0f0 0%, #eff0ff 21.4%, #ffffff 48.5%, #eff0ff 76.7%, #ccf0f0 100%);
+	background: #F9F9FE;
+	// background-image: linear-gradient(180deg, #ccf0f0 0%, #eff0ff 21.4%, #ffffff 48.5%, #eff0ff 76.7%, #ccf0f0 100%);
 	.title{
 		font-size: 50rpx;
 		color:#000;
@@ -135,25 +145,30 @@
 		line-height: 1.4em;
 	}
 	.info{
-		font-size: 30rpx;
+		background: #efefef;
+		border-radius: 15rpx;
+		padding:20rpx;
+		font-size: 25rpx;
 		color:#666;
-		padding:30rpx 0 60rpx;
-		text{
-			padding-right: 30rpx;
-		}
+		display: flex;
+		justify-content: space-between;
+		margin:40rpx 0;
 	}
 	.content{
 		font-size: 36rpx;
 		line-height: 1.7em;
-		
-		
-	}
-	.picurls{
-		padding-top: 50rpx;
 		image{
+			padding-top: 50rpx;
 			width: 100%;
 			display: block;
 			margin-bottom:30rpx;
+		}
+		
+	}
+	.picurls{
+		
+		image{
+			
 		}
 	}
 	.btnGroup{
