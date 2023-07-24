fadd4197a9943cdc72e975adb8798366773c607c
sunheran <1131463196@qq.com>
2023-07-24 09:23:01 +0800

    首页改_id,文章详情页修改

--- pages/wzdetail/wzdetail.vue
+++ pages/wzdetail/wzdetail.vue
@@ -1,112 +1,166 @@
 <template>
-  <view class="news-detail">
-    <view class="title">{{title}}</view>
-    <view class="info">
-      <text class="author">{{author}}</text>
-      <text class="time">{{time}}</text>
-    </view>
-
-	<text class="time">{{content}}</text>
-    <rich-text :nodes="content"></rich-text>
-
-    <view class="image-wrap">
-      <image :src="imageUrl" mode="widthFix"></image>
-    </view>
-  </view> 
+	<view class="detail" >		
+		<view v-if="loadState">			
+			<view class="title">{{detail.title}}</view>
+			<view class="info">
+				<text>{{detail.author}}</text>
+				<text>			
+					<uni-dateformat 
+					:date="detail.posttime" 
+					format="yyyy年MM月dd日 hh:mm:ss" 
+					></uni-dateformat>
+				</text>
+			</view>
+			<view class="content">
+				{{detail.content}}
+			</view>
+			
+			<view class="picurls" v-if="detail.picurls && detail.picurls.length">
+				<image v-for="item in detail.picurls" :src="item" mode="widthFix"></image>
+			</view>
+			
+			<view class="btnGroup">
+				<button size="mini" @click="goEdit">修改</button>
+				<button size="mini" type="warn" @click="onRemove">删除</button>
+			</view>
+		</view>
+		
+		<view v-else>
+			<uni-load-more status="loading"></uni-load-more>
+		</view>
+		
+		
+	</view>
 </template>
 
 <script>
 	let id;
-export default {
-  data() {
-    return {
-	  // objData:{},
-	  // id:1,
-	  
-	  detail:{},
-	  loadState:false,
-	  
-      title: '新闻标题新闻标题新闻标题新闻标题新闻标题',
-      author: '张三',
-      time: '2023-01-01 10:00:00',
-      content: '<p>新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文新闻正文</p>',
-      imageUrl: "/static/lb3.jpg"
-    };
-  },
-  onLoad(e){//接受了传过来的id
-  	console.log(e)
-  	id=e.id
-  	this.getDetail();
-  },
-  methods:{
-	  //获取详情
-		getDetail(){
-			uniCloud.callFunction({
-				name:"art_get_row",
-				data:{
-					id
-				}
-			}).then(res=>{						
-				this.detail=res.result.data[0]
-				this.loadState=true
-				uni.setNavigationBarTitle({
-					title:this.detail.title
-				})
-				
-			})
+	export default {
+		data() {
+			return {
+				detail:{},
+				loadState:false
+			};
+		},
+		onLoad(e){			
+			id=e.id
 			
+		},
+		onShow(){
+			this.getDetail();
+		},
+		methods:{
+			//跳转到修改页面
+			goEdit(){
+				uni.navigateTo({
+					url:"/pages/edit/edit?id="+id
+				})
+			},
+			
+			//删除一条记录
+			onRemove(){
+				uni.showModal({
+					content:"是否确认删除？",
+					success:res=>{						
+						if(res.confirm){
+							this.removeFun()
+						}
+					}					
+				})			
+				
+			},
+			
+			removeFun(){
+				uniCloud.callFunction({
+					name:"art_remove_row",
+					data:{
+						id
+					}
+				}).then(res=>{
+					uni.showToast({
+						title:"删除成功~"
+					})
+					setTimeout(()=>{
+						uni.reLaunch({
+							url:"/pages/index/index"
+						})
+					},800)
+				})
+			},
+			
+			
+			
+			//获取详情
+			getDetail(){
+				uniCloud.callFunction({
+					name:"art_get_row",
+					data:{
+						id
+					}
+				}).then(res=>{						
+					this.detail=res.result.data[0]
+					this.loadState=true
+					uni.setNavigationBarTitle({
+						title:this.detail.title
+					})
+				}).catch(()=>{
+					uni.showToast({
+						icon:"error",
+						title:"删除有误"
+					})
+					setTimeout(()=>{
+						uni.reLaunch({
+							url:"/pages/index/index"
+						})
+					},800)
+					
+				})
+			}
 		}
-  	// getDetail(){
-  	// 	// uni.showLoading({
-  	// 	// 	title:"数据加载中...",
-  	// 	// 	mask:true
-  	// 	// })
-  	// 	uni.request({
-  	// 		url:"https://fc-mp-836eb6c6-0a4f-47c9-8149-6d32fa5245cd.next.bspapp.com/getArticleSingal/"+this.id,
-  	// 		success:res=>{
-  	// 			console.log(res)
-  	// 			this.objData=res.data.data
-  	// 		}
-  	// 	})
-  	// }
-  }
-};
+		
+	}
 </script>
 
-<style scoped lang="scss">
-.news-detail{
+<style lang="scss" scoped>
+	.page{
+		
+	}
+.detail{
 	padding:30rpx;
-	.title {
-		font-size: 46rpx;
-		color:#333;
-		font-weight: bold;
+	background-image: linear-gradient(180deg, #ccf0f0 0%, #eff0ff 21.4%, #ffffff 48.5%, #eff0ff 76.7%, #ccf0f0 100%);
+	.title{
+		font-size: 50rpx;
+		color:#000;
+		text-align: justify;
+		line-height: 1.4em;
 	}
-
-	.info {
-	  background: #F6F6F6;
-	  padding:20rpx;
-	  font-size: 25rpx;
-	  color:#666;
-	  display: flex;
-	  justify-content: space-between;
-	  margin:40rpx 0;
+	.info{
+		font-size: 30rpx;
+		color:#666;
+		padding:30rpx 0 60rpx;
+		text{
+			padding-right: 30rpx;
+		}
 	}
-
-	.content {
-	  padding-bottom:50rpx;
-	  img{
-	  	max-width: 100%;
-	  }
+	.content{
+		font-size: 36rpx;
+		line-height: 1.7em;
+		
+		
 	}
-
-
-
-	.image-wrap {
-	  width: 100%;
+	.picurls{
+		padding-top: 50rpx;
+		image{
+			width: 100%;
+			display: block;
+			margin-bottom:30rpx;
+		}
 	}
-
-	image {
-	  width: 100%;
+	.btnGroup{
+		padding:50rpx 0;	
+		button{
+			margin-right: 30rpx;
+		}
 	}
 }
 </style>
